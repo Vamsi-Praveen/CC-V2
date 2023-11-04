b0284b0a4afc646a987d362c9aacfb429beeefdd
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2023-11-04 22:47:24 +0530

    error

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
   return (
     <section>
       <Header/>
-      <div className='flex items-center w-full h-[480px] justify-center pt-[10px]'>
+      <div className='flex items-center w-full h-[480px] justify-center pt-[10px] pb-[50px]'>
         <img src={error} alt='Error Page Not Found 404' className='h-[480px] pt-[40px]'/>
       </div>
       <Footer/>
